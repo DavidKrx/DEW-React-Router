24c45b88f43a9dc1896cab4803227379f07528e9
2dawa <2dawa@informatica.edu>
2024-11-07 12:14:44 +0000

    Cosas clase

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,22 @@ function App() {
     {text: 'Pay the gym', done: true},
     {text: 'Go to gym', done: false}
   ]
-
+  let programadores = [
+    {
+        nombre: 'Antonio', apellido1: 'González', apellido2: 'González', edad: 25, experiencia: 4,
+        lenguajes: ['C++', 'JS', 'PHP']
+    },
+    {
+        nombre: 'Juana', apellido1: 'Pérez', apellido2: 'Díaz', edad: 30, experiencia: 5, lenguajes:
+            ['C', 'JS', 'Java']
+    },
+    {
+        nombre: 'Pedro', apellido1: 'Abad', apellido2: 'García', edad: 24, experiencia: 15, lenguajes:
+            ['Python', 'JS', 'Java', 'C++']
+    }
+   
+]
+const uwu="Menor de edad => Carcel"
   return (
     <>
       <div className='app'>
@@ -27,6 +42,15 @@ function App() {
               </li>
               ))}
           </ul>
+          <ul>
+            {programadores.map( ({nombre,experiencia}, i)=>(
+              <li key={nombre}>
+                {nombre}   {experiencia >4 ? <button>{uwu}</button>:<button>:D</button>}
+                
+              </li>
+              ))}
+          </ul>
+          <iframe><img src="" alt="" /></iframe>
           <button className='wish-clear' type='button'>Archive done</button>
       </div>
     </>
